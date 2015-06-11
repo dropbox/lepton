28c32710f8a1cf40b05307b3ec2b0aa4bc83b063
Keith Winstein <keithw@mit.edu>
2015-06-11 16:19:46 -0700

    vp8_decoder: Use more C++

--- src/vp8_decoder.cc
+++ src/vp8_decoder.cc
@@ -1,19 +1,13 @@
 /* -*-mode:c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
 #include <algorithm>
+#include <cassert>
+
 #include "bitops.hh"
 #include "component_info.hh"
 #include "uncompressed_components.hh"
 #include "jpgcoder.hh"
 #include "vp8_decoder.hh"
-VP8ComponentDecoder::VP8ComponentDecoder() {
-    str_in = NULL;
-    for (int i = 0; i < 4; ++i) {
-        cur_read_batch[i] = 0;
-        started_scan[i] = false;
-    }
-    cmp = 0;
-}
 
 void VP8ComponentDecoder::initialize(iostream *input) {
     str_in = input;
@@ -23,7 +17,8 @@ CodingReturnValue VP8ComponentDecoder::decode_chunk(UncompressedComponents *dst)
     return vp8_decoder(dst, str_in);
 }
 
-void VP8ComponentDecoder::vp8_continuous_decoder(UncompressedComponents* colldata, iostream *str_in) {
+void VP8ComponentDecoder::vp8_continuous_decoder( UncompressedComponents * const colldata,
+                                                  iostream * const str_in) {
     colldata->worker_wait_for_begin_signal();
 
     VP8ComponentDecoder scd;
@@ -32,48 +27,66 @@ void VP8ComponentDecoder::vp8_continuous_decoder(UncompressedComponents* colldat
         
     }
 }
-CodingReturnValue VP8ComponentDecoder::vp8_decoder(UncompressedComponents* colldata, iostream *str_in) {
-    
-	char ujpg_mrk[ 64 ] = "CMP";
+
+bool VP8ComponentDecoder::started_scan() const
+{
+    return started_scan_.at( current_component_ );
+}
+
+void VP8ComponentDecoder::set_started_scan()
+{
+    started_scan_.at( current_component_ ) = true;
+}
+
+CodingReturnValue VP8ComponentDecoder::vp8_decoder( UncompressedComponents * const colldata,
+                                                     iostream * const str_in) {    
+    /* verify JFIF is Y' or Y'CbCr */
+    assert( cmpc >= 1 );
+    assert( cmpc <= 3 );
+
+    char ujpg_mrk[ 64 ] = "CMP";
     int batch_size = 1600;
-    colldata->worker_update_band_progress(64); // we are optimizing for baseline only atm
-    colldata->worker_update_bit_progress(16); // we are optimizing for baseline only atm
+    colldata->worker_update_band_progress( 64 ); // we are optimizing for baseline only atm
+    colldata->worker_update_bit_progress( 16 ); // we are optimizing for baseline only atm
+
 	// read actual decompressed coefficient data from file
-    if (!started_scan[cmp]) {
-        if (cmp != 0) {
+    if ( not started_scan() ) {
+        if (current_component_ != 0) {
             str_in->read( ujpg_mrk, 1, 4 );
         } else {
             str_in->read( ujpg_mrk + 3, 1, 1 );
         }
-        started_scan[cmp] = true;
+
+        set_started_scan();
+
         // check marker
         if (strncmp( ujpg_mrk, "CMP", 3 ) != 0) {
-            sprintf( errormessage, "CMP%i marker not found", cmp );
+            sprintf( errormessage, "CMP%i marker not found", current_component_ );
             errorlevel = 2;    
             return CODING_ERROR;
         }
     }
     {
         // read coefficient data from file
-        signed short * start = colldata->full_component_write( cmp );
-        int target = colldata->component_size_in_blocks(cmp);
-        while (cur_read_batch[cmp] < target) {
-            int cur_read_size = std::min(batch_size, target - cur_read_batch[cmp]);
-            ssize_t retval = str_in->read(start + cur_read_batch[cmp] * 64 , int(sizeof( short ) * 64), cur_read_size);
+        signed short * start = colldata->full_component_write( current_component_ );
+        int target = colldata->component_size_in_blocks(current_component_);
+        while (cur_read_batch_[current_component_] < target) {
+            int cur_read_size = std::min(batch_size, target - cur_read_batch_[current_component_]);
+            ssize_t retval = str_in->read(start + cur_read_batch_[current_component_] * 64 , int(sizeof( short ) * 64), cur_read_size);
             if (retval != cur_read_size) {
                 sprintf( errormessage, "unexpected end of file blocks %ld !=  %d", retval, cur_read_size);
                 errorlevel = 2;
                 return CODING_ERROR;
             }
-            cur_read_batch[cmp] += cur_read_size;
-            colldata->worker_update_cmp_progress(cmp, cur_read_size);
-            if (cur_read_batch[cmp] < target) {
+            cur_read_batch_[current_component_] += cur_read_size;
+            colldata->worker_update_cmp_progress(current_component_, cur_read_size);
+            if (cur_read_batch_[current_component_] < target) {
                 return CODING_PARTIAL;
             }
         }
     }
-    if (cmp < cmpc) {
-        ++cmp;
+    if (current_component_ < cmpc) {
+        ++current_component_;
         return CODING_PARTIAL;
     } else {
         return CODING_DONE;
--- src/vp8_decoder.hh
+++ src/vp8_decoder.hh
@@ -2,15 +2,22 @@
 #include "base_coders.hh"
 
 class VP8ComponentDecoder : public BaseDecoder {
-    iostream * str_in;
-    bool started_scan[4];
-    int cmp;
-    int cur_read_batch[4];
+    iostream * str_in {};
+
+    std::array<bool, 4> started_scan_ {{}};
+    unsigned char current_component_ {};
+    std::array<int, 4> cur_read_batch_ {{}};
+
+    bool started_scan() const;
+    void set_started_scan();
+
 public:
-    VP8ComponentDecoder();
+    VP8ComponentDecoder() {}
     void initialize(iostream *input);
-    CodingReturnValue vp8_decoder(UncompressedComponents* colldata, iostream *str_in);
-    static void vp8_continuous_decoder(UncompressedComponents* colldata, iostream *str_in);
+    CodingReturnValue vp8_decoder( UncompressedComponents * const colldata,
+                                   iostream * const str_in );
+    static void vp8_continuous_decoder( UncompressedComponents * const colldata,
+                                        iostream * const str_in);
     //necessary to implement the BaseDecoder interface. Thin wrapper around vp8_decoder
     virtual CodingReturnValue decode_chunk(UncompressedComponents*dst);
 };
