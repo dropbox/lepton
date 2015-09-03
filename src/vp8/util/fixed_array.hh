966b33ece2f86e714517c4534ce63bf09e70d471
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2015-09-03 01:28:14 -0700

    bye 2d

--- src/vp8/util/fixed_array.hh
+++
@@ -1,7 +0,0 @@
-#ifndef _FIXED_ARRAY_HH_
-#define _FIXED_ARRAY_HH_
-#include <array>
-#include <assert.h>
-#include <cstring>
-template <class T, size_t dim> using FixedArray = std::array<T, dim>;
-#endif /* ARRAY_HH */
