f8b2d2431e472b5e6b722be9e2eb2bda3fb25b19
Daniel Reiter Horn <danielrh@dropbox.com>
2015-09-11 22:11:09 -0700

    limiting to 0-255

--- src/vp8/model/branch.hh
+++ src/vp8/model/branch.hh
@@ -8,8 +8,8 @@ typedef uint8_t Probability;
 class Branch
 {
 private:
-  uint16_t false_count_ = 1, true_count_ = 1;
   Probability probability_ = 128;
+  uint8_t false_count_ = 1, true_count_ = 1;
   friend class JpegBoolDecoder;
   friend class JpegBoolEncoder;
 public:
@@ -18,22 +18,18 @@ public:
   uint32_t true_count() const { return true_count_; }
   uint32_t false_count() const { return false_count_; }
   
-  void record_true( void ) { true_count_ = true_count_ + 1; }
-  void record_false( void ) { false_count_ = false_count_ + 1; }
   void record_true_and_update( void ) {
-      if (true_count_ & 0xfe00) { // check less than 512
+      if (true_count_ == 0xff) { // check less than 512
           normalize();
-      } else {
-          true_count_ += 1;
       }
+      true_count_ += 1;
       probability_ = optimize();
   }
   void record_false_and_update( void ) {
-      if (false_count_ & 0xfe00) {// 2x the size of prob
+      if (false_count_ == 0xff) {// 2x the size of prob
           normalize();
-      } else {
-          false_count_ += 1;
       }
+      false_count_ += 1;
       probability_ = optimize();
   }
   void normalize() {
