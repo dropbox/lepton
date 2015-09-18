89aeaa16f9134bc20c0a823cfeb1a7192278b71f
Daniel Reiter Horn <danielrh@dropbox.com>
2015-09-17 22:59:13 -0700

    provide an alternate method to update the arithmetic coder that uses the probability alone to track state updates by having probabilities exponentially distributed across the number line from .5 to .5 * base**128. This resulting probability/update can come from a single table lookup

--- src/vp8/model/branch.hh
+++ src/vp8/model/branch.hh
@@ -1,18 +1,22 @@
 #ifndef _BRANCH_HH_
 #define _BRANCH_HH_
 #include "numeric.hh"
+#include <cmath>
 typedef uint8_t Probability;
 
 //#define VP8_ENCODER 1
 
 //#define JPEG_ENCODER
 // ^^^ if we want to try to use the JPEG spec arithmetic coder, uncomment above
-
+//#define USE_COUNT_FREE_UPDATE
 class Branch
 {
 private:
   uint8_t counts_[2] = {1, 1};
-    Probability probability_ = 128;
+  Probability probability_ = 128;
+#ifdef USE_COUNT_FREE_UPDATE
+  uint8_t lprob = 128;
+#endif
   friend class JpegBoolDecoder;
   friend class JpegBoolEncoder;
 public:
@@ -20,6 +24,7 @@ public:
 
   uint32_t true_count() const { return counts_[1]; }
   uint32_t false_count() const { return counts_[0]; }
+
     __attribute__((always_inline))
   void record_obs_and_update(bool obs) {
       unsigned int fcount = counts_[0];
@@ -33,6 +38,55 @@ public:
       } else {
           probability_ = optimize(fcount + tcount + 1);
       }
+#ifdef USE_COUNT_FREE_UPDATE
+      int limit = 120;
+      if (lprob == 128 && obs) {
+          lprob = 127;
+      } else if (lprob == 127 && !obs) {
+          lprob = 128;
+      } else {
+          if (lprob > 127 && !obs) {
+              if (lprob < 128 + 40) {
+                  lprob += 1;
+              } else if (lprob < 128 + 96) {
+                  lprob += 2;
+              } else {
+                  lprob += 3;
+              }
+              if (lprob > 127 + limit) lprob = 127 + limit;
+          } else if (lprob < 128 && obs) {
+              if (lprob > 127 - 40) {
+                  lprob -= 1;
+              } else if (lprob > 127 - 96) {
+                  lprob -= 2;
+              } else {
+                  lprob -=3;
+              }
+              if (lprob < 128 - limit) {
+                  lprob = 128 - limit;
+              }
+          } else {
+              uint8_t pval = obs ? lprob - 128 : 127 - lprob;
+              double base = .9575;
+              double prob = 0.5 * pow(base, (double)pval);
+              double new_prob = base * prob + 1 - base;
+              int search_result = pval - 1;
+              double best_search_dist = fabs(0.5 * pow(base, (double)search_result) - new_prob);
+              for (int search = search_result - 1; search > 0; --search) {
+                  double search_prob = 0.5 * pow(base, (double)search);
+                  if (fabs(search_prob - new_prob) < best_search_dist) {
+                      search_result =  search;
+                      best_search_dist = fabs(search_prob - new_prob);
+                  } else break;
+              }
+              lprob = obs ? search_result + 128 : 127 - search_result;
+              probability_ = new_prob * 256;
+              if (obs) {
+                  probability_ = 255 - probability_;
+              }
+          }
+      }
+#endif
   }
   void normalize() {
       counts_[0] = ((1 + (unsigned int)counts_[0]) >> 1);
