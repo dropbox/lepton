de29cb476c9f8087c2b17d5222fd10b481462901
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2015-09-27 04:01:05 -0700

    added event-driven approach so that we don't spin on any locks and the atomics are only for enforcement of memory ordering

--- src/vp8/util/generic_worker.cc
+++ src/vp8/util/generic_worker.cc
@@ -1,8 +1,15 @@
 #include <emmintrin.h>
 #include <assert.h>
+#include <unistd.h>
+#include <errno.h>
 #include "generic_worker.hh"
-
+const bool use_pipes = true;
 void GenericWorker::wait_for_work() {
+    char data = 0;
+    if (use_pipes) {
+        while (read(new_work_pipe[0], &data, 1) < 0 && errno == EINTR) {
+        }
+    }
     while(!new_work_exists_.load(std::memory_order_relaxed)) {
         _mm_pause();
     }
@@ -12,10 +19,46 @@ void GenericWorker::wait_for_work() {
         assert(false);// invariant violated
     }
     work_done_++;
+    data = 1;
+    if (use_pipes) {
+        while (write(work_done_pipe[1], &data, 1) < 0 && errno == EINTR) {
+        
+        }
+    }
 }
 
+bool GenericWorker::is_done() {
+        if (work_done_.load(std::memory_order_relaxed) > 0) {
+            return work_done_.load() != 0; // enforce memory ordering
+        }
+        return false;
+    }
+
+void GenericWorker::activate_work() {
+    ++new_work_exists_;
+    char data = 0;
+    while (write(new_work_pipe[1], &data, 1) < 0 && errno == EINTR) {
+        
+    }
+}
+Sirikata::Array1d<int, 2> GenericWorker::initiate_pipe(){
+    int pipes[2] = {-1, -1};
+    if (use_pipes) {
+        while (pipe(pipes) != 0 && errno == EINTR){
+        }
+    }
+    Sirikata::Array1d<int, 2> retval;
+    retval.at(0) = pipes[0];
+    retval.at(1) = pipes[1];
+    return retval;
+}
 void GenericWorker::main_wait_for_done() {
     assert(new_work_exists_.load()); // make sure this has work to do
+    if (use_pipes) {
+        char data = 0;
+        while (read(work_done_pipe[0], &data, 1) < 0 && errno == EINTR) {
+        }
+    }
     
     while(!is_done()) {
         _mm_pause();
--- src/vp8/util/generic_worker.hh
+++ src/vp8/util/generic_worker.hh
@@ -1,29 +1,27 @@
 #include <atomic>
 #include <functional>
 #include <thread>
+#include "nd_array.hh"
 #include "options.hh"
 
 struct GenericWorker {
     std::atomic<int> new_work_exists_;
     std::atomic<int> work_done_;
     std::function<void()> work;
-    
+    Sirikata::Array1d<int, 2> new_work_pipe;
+    Sirikata::Array1d<int, 2> work_done_pipe;
+    static Sirikata::Array1d<int, 2> initiate_pipe();
     GenericWorker() : new_work_exists_(0),
                       work_done_(0),
+                      new_work_pipe(initiate_pipe()),
+                      work_done_pipe(initiate_pipe()),
                       child_(std::bind(&GenericWorker::wait_for_work,
                                            this)) {
     }
-    void activate_work() {
-        ++new_work_exists_;
-    }
-    bool is_done() {
-        if (work_done_.load(std::memory_order_relaxed) > 0) {
-            return work_done_.load() != 0; // enforce memory ordering
-        }
-        return false;
-    }
+    void activate_work();
+    bool is_done();
     void main_wait_for_done();
     void wait_for_work();
 private:
-    std::thread child_;
+    std::thread child_; // this must come after other members, so items are initialized first
 };
