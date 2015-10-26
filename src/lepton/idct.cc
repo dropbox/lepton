a53e02d319b2c7f2a3bfa9f08ddccef261c288f8
Daniel Reiter Horn <danielrh@dropbox.com>
2015-10-25 21:40:35 -0700

    remove dead code

--- src/lepton/idct.cc
+++ src/lepton/idct.cc
@@ -163,21 +163,11 @@ template<int offset, int stride> __m128i vquantize(int which_vec, __m128i vec, c
 
 
 __m128i epi32l_to_epi16(__m128i lowvec) {
-/*    int16_t a,b,c,d;
-    a = _mm_extract_epi32(lowvec,0);
-    b = _mm_extract_epi32(lowvec,1);
-    c = _mm_extract_epi32(lowvec,2);
-    d = _mm_extract_epi32(lowvec,3);
-    return _mm_set_epi16(0,0,0,0,d,c,b,a);
- */
     return _mm_shuffle_epi8(lowvec, _mm_set_epi8(0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
                                                  0xd, 0xc, 0x9, 0x8, 0x5, 0x4, 0x1, 0x0));
 }
 void idct(const AlignedBlock &block, const uint16_t q[64], int16_t voutp[64], bool ignore_dc) {
-    int16_t outp[64];
-    char intermed_storage[64 * sizeof(int32_t) + 16];
-    // align intermediate storage to 16 bytes
-    int32_t *intermed = (int32_t*) (intermed_storage + 16 - ((intermed_storage - (char*)nullptr)&0xf));
+    
     char vintermed_storage[64 * sizeof(int32_t) + 16];
     // align intermediate storage to 16 bytes
     int32_t *vintermed = (int32_t*) (vintermed_storage + 16 - ((vintermed_storage - (char*)nullptr) &0xf));
@@ -217,82 +207,6 @@ void idct(const AlignedBlock &block, const uint16_t q[64], int16_t voutp[64], bo
         xv5 = vquantize<7, 8>(yvec, xv5, q);
         xv6 = vquantize<5, 8>(yvec, xv6, q);
         xv7 = vquantize<3, 8>(yvec, xv7, q);
-        int32_t xvec0[4];
-        int32_t xvec1[4];
-        int32_t xvec2[4];
-        int32_t xvec3[4];
-        int32_t xvec4[4];
-        int32_t xvec5[4];
-        int32_t xvec6[4];
-        int32_t xvec7[4];
-        int32_t xvec8[4];
-#define x0 xvec0[y&3]
-#define x1 xvec1[y&3]
-#define x2 xvec2[y&3]
-#define x3 xvec3[y&3]
-#define x4 xvec4[y&3]
-#define x5 xvec5[y&3]
-#define x6 xvec6[y&3]
-#define x7 xvec7[y&3]
-#define x8 xvec8[y&3]
-        for (int y = yvec/8; y < yvec/8 + 4; ++y) {
-        int y8 = y * 8;
-         x0 = (((ignore_dc && y == 0)
-                       ? 0 : (block.coefficients_raster(y8 + 0) * q[y8 + 0]) << 11)) + 128;
-         x1 =(block.coefficients_raster(y8 + 4) * q[y8 + 4]) << 11;
-         x2 = block.coefficients_raster(y8 + 6) * q[y8 + 6];
-         x3 = block.coefficients_raster(y8 + 2) * q[y8 + 2];
-         x4 = block.coefficients_raster(y8 + 1) * q[y8 + 1];
-         x5 = block.coefficients_raster(y8 + 7) * q[y8 + 7];
-         x6 = block.coefficients_raster(y8 + 5) * q[y8 + 5];
-         x7 = block.coefficients_raster(y8 + 3) * q[y8 + 3];
-            assert(x0 == _mm_extract_epi32(xv0, y&3));
-            assert(x1 == _mm_extract_epi32(xv1, y&3));
-            assert(x2 == _mm_extract_epi32(xv2, y&3));
-            assert(x3 == _mm_extract_epi32(xv3, y&3));
-            assert(x4 == _mm_extract_epi32(xv4, y&3));
-            assert(x5 == _mm_extract_epi32(xv5, y&3));
-            assert(x6 == _mm_extract_epi32(xv6, y&3));
-            assert(x7 == _mm_extract_epi32(xv7, y&3));
-        // Prescale.
-
-        // Stage 1.
-         x8 = w7 * (x4 + x5);
-        x4 = x8 + w1mw7*x4;
-        x5 = x8 - w1pw7*x5;
-        x8 = w3 * (x6 + x7);
-        x6 = x8 - w3mw5*x6;
-        x7 = x8 - w3pw5*x7;
-
-        // Stage 2.
-        x8 = x0 + x1;
-        x0 -= x1;
-        x1 = w6 * (x3 + x2);
-        x2 = x1 - w2pw6*x2;
-        x3 = x1 + w2mw6*x3;
-        x1 = x4 + x6;
-        x4 -= x6;
-        x6 = x5 + x7;
-        x5 -= x7;
-
-        // Stage 3.
-        x7 = x8 + x3;
-        x8 -= x3;
-        x3 = x0 + x2;
-        x0 -= x2;
-        x2 = (r2*(x4+x5) + 128) >> 8;
-        x4 = (r2*(x4-x5) + 128) >> 8;
-
-        // Stage 4.
-        intermed[y8+0] = (x7 + x1) >> 8;
-        intermed[y8+1] = (x3 + x2) >> 8;
-        intermed[y8+2] = (x0 + x4) >> 8;
-        intermed[y8+3] = (x8 + x6) >> 8;
-        intermed[y8+4] = (x8 - x6) >> 8;
-        intermed[y8+5] = (x0 - x4) >> 8;
-        intermed[y8+6] = (x3 - x2) >> 8;
-        intermed[y8+7] = (x7 - x1) >> 8;
-        }
         // Stage 1.
         xv8 = _mm_mullo_epi32(_mm_set1_epi32(w7), _mm_add_epi32(xv4, xv5));
         xv4 = _mm_add_epi32(xv8, _mm_mullo_epi32(_mm_set1_epi32(w1mw7), xv4));
@@ -353,30 +267,9 @@ void idct(const AlignedBlock &block, const uint16_t q[64], int16_t voutp[64], bo
         }
 
     }
-    for (int i= 0; i < 64; ++i) {
-        assert(intermed[i] == vintermed[i]);
-    }
     // Vertical 1-D IDCT.
     for (uint8_t xvec = 0; xvec < 8; xvec += 4) {
         __m128i yv0, yv1, yv2, yv3, yv4, yv5, yv6, yv7, yv8;
-        int32_t yvec0[4];
-        int32_t yvec1[4];
-        int32_t yvec2[4];
-        int32_t yvec3[4];
-        int32_t yvec4[4];
-        int32_t yvec5[4];
-        int32_t yvec6[4];
-        int32_t yvec7[4];
-        int32_t yvec8[4];
-#define y0 yvec0[x&3]
-#define y1 yvec1[x&3]
-#define y2 yvec2[x&3]
-#define y3 yvec3[x&3]
-#define y4 yvec4[x&3]
-#define y5 yvec5[x&3]
-#define y6 yvec6[x&3]
-#define y7 yvec7[x&3]
-#define y8 yvec8[x&3]
         yv0 = _mm_add_epi32(_mm_slli_epi32(_mm_load_si128((const __m128i*)(vintermed + xvec)), 8),
                             _mm_set1_epi32(8192));
         yv1 = _mm_slli_epi32(_mm_load_si128((const __m128i*)(vintermed + 8 * 4 + xvec)), 8);
@@ -386,65 +279,6 @@ void idct(const AlignedBlock &block, const uint16_t q[64], int16_t voutp[64], bo
         yv5 = _mm_load_si128((const __m128i*)(vintermed + 8 * 7 + xvec));
         yv6 = _mm_load_si128((const __m128i*)(vintermed + 8 * 5 + xvec));
         yv7 = _mm_load_si128((const __m128i*)(vintermed + 8 * 3 + xvec));
-    for (int32_t x = xvec; x < xvec + 4; ++x) {
-        // Similar to the horizontal 1-D IDCT case, if all the AC components are zero, then the IDCT is trivial.
-        // However, after performing the horizontal 1-D IDCT, there are typically non-zero AC components, so
-        // we do not bother to check for the all-zero case.
-
-        // Prescale.
-         y0 = (intermed[8*0+x] << 8) + 8192;
-         y1 = intermed[8*4+x] << 8;
-         y2 = intermed[8*6+x];
-         y3 = intermed[8*2+x];
-         y4 = intermed[8*1+x];
-         y5 = intermed[8*7+x];
-         y6 = intermed[8*5+x];
-         y7 = intermed[8*3+x];
-        assert(y0 == _mm_extract_epi32(yv0, x & 3));
-        assert(y1 == _mm_extract_epi32(yv1, x & 3));
-        assert(y2 == _mm_extract_epi32(yv2, x & 3));
-        assert(y3 == _mm_extract_epi32(yv3, x & 3));
-        assert(y4 == _mm_extract_epi32(yv4, x & 3));
-        assert(y5 == _mm_extract_epi32(yv5, x & 3));
-        assert(y6 == _mm_extract_epi32(yv6, x & 3));
-        assert(y7 == _mm_extract_epi32(yv7, x & 3));
-        // Stage 1.
-         y8 = w7*(y4+y5) + 4;
-        y4 = (y8 + w1mw7*y4) >> 3;
-        y5 = (y8 - w1pw7*y5) >> 3;
-        y8 = w3*(y6+y7) + 4;
-        y6 = (y8 - w3mw5*y6) >> 3;
-        y7 = (y8 - w3pw5*y7) >> 3;
-
-        // Stage 2.
-        y8 = y0 + y1;
-        y0 -= y1;
-        y1 = w6*(y3+y2) + 4;
-        y2 = (y1 - w2pw6*y2) >> 3;
-        y3 = (y1 + w2mw6*y3) >> 3;
-        y1 = y4 + y6;
-        y4 -= y6;
-        y6 = y5 + y7;
-        y5 -= y7;
-
-        // Stage 3.
-        y7 = y8 + y3;
-        y8 -= y3;
-        y3 = y0 + y2;
-        y0 -= y2;
-        y2 = (r2*(y4+y5) + 128) >> 8;
-        y4 = (r2*(y4-y5) + 128) >> 8;
-
-        // Stage 4.
-        outp[8*0+x] = (y7 + y1) >> 11;
-        outp[8*1+x] = (y3 + y2) >> 11;
-        outp[8*2+x] = (y0 + y4) >> 11;
-        outp[8*3+x] = (y8 + y6) >> 11;
-        outp[8*4+x] = (y8 - y6) >> 11;
-        outp[8*5+x] = (y0 - y4) >> 11;
-        outp[8*6+x] = (y3 - y2) >> 11;
-        outp[8*7+x] = (y7 - y1) >> 11;
-    }
         // Stage 1.
         yv8 = _mm_add_epi32(_mm_mullo_epi32(_mm_add_epi32(yv4, yv5), _mm_set1_epi32(w7)), _mm_set1_epi32(4));
         yv4 = _mm_srai_epi32(_mm_add_epi32(yv8, _mm_mullo_epi32(_mm_set1_epi32(w1mw7), yv4)), 3);
@@ -491,10 +325,5 @@ void idct(const AlignedBlock &block, const uint16_t q[64], int16_t voutp[64], bo
         _mm_storel_epi64((__m128i*)(char*)(voutp + 5 * 8 + xvec), epi32l_to_epi16(row5));
         _mm_storel_epi64((__m128i*)(char*)(voutp + 6 * 8 + xvec), epi32l_to_epi16(row6));
         _mm_storel_epi64((__m128i*)(char*)(voutp + 7 * 8 + xvec), epi32l_to_epi16(row7));
-        for (int i = 0; i < 8; ++i) {
-            for (int v = 0; v < 4; ++v) {
-                assert(voutp[xvec + v + i * 8] == outp[xvec + v + i * 8]);
-            }
-        }
     }
 }
