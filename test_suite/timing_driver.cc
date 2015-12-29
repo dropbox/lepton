4f36959567c6e8ce237f40f3498314b0a16ac3ec
Daniel Reiter Horn <danielrh@dropbox.com>
2015-12-29 06:36:03 +0000

    fix the test to properly preload the encoder and then we can time the whole thing

--- test_suite/timing_driver.cc
+++ test_suite/timing_driver.cc
@@ -218,8 +218,8 @@ int run_test(const std::vector<unsigned char> &testImage,
     if (expect_failure || expect_decoder_failure) {
         signal(SIGPIPE, SIG_IGN);
     }
-    const char * encode_args[MAX_ARGS] = {"./lepton", "-hugepages", "-timing=test_timing", NULL};
-    const char * decode_args[MAX_ARGS] = {"./lepton", "-hugepages", NULL};
+    const char * encode_args[MAX_ARGS] = {"./lepton", "-hugepages", "-decode", NULL};
+    const char * decode_args[MAX_ARGS] = {"./lepton", "-hugepages", "-recode", NULL};
     if (!use_lepton) {
         encode_args[get_last_arg(encode_args)] = "-ujg";
     }
@@ -335,7 +335,7 @@ int run_test(const std::vector<unsigned char> &testImage,
     do {
         status = close(decode_stdout[1]);
     } while (status == -1 && errno == EINTR);
-    bool presend_header = true;
+    bool presend_header = false;
     if (!presend_header) {
         sleep_a_bit();
     }
