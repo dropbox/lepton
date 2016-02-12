dfce4a752d780146efde4ef8e3decb6ca41503c6
Daniel Reiter Horn <danielrh@dropbox.com>
2016-02-11 22:52:47 -0800

    added missing file

---
+++ src/io/BoundedMemWriter.hh
@@ -0,0 +1,55 @@
+#include "Reader.hh"
+namespace Sirikata {
+class SIRIKATA_EXPORT BoundedMemWriter : public Sirikata::DecoderWriter {
+    std::vector<Sirikata::uint8, JpegAllocator<uint8_t> > mBuffer;
+    size_t mWriteCursor;
+  public:
+    BoundedMemWriter(const JpegAllocator<uint8_t> &alloc) : mBuffer(alloc){
+        mWriteCursor = 0;
+    }
+    void set_bound(size_t bound) {
+        mBuffer.resize(bound);
+        mWriteCursor = std::min(bound, mWriteCursor);
+    }
+    void Reset() {
+        mWriteCursor = 0;
+    }
+    void Close() {
+        mWriteCursor = 0;
+    }
+    virtual std::pair<Sirikata::uint32, Sirikata::JpegError> Write(const Sirikata::uint8*data,
+                                                                   unsigned int size) {
+        unsigned int bounded_size = 0;
+        if (!has_reached_bound()) {
+            bounded_size = (unsigned int)std::min((size_t)size,
+                                                               mBuffer.size() - mWriteCursor);
+            memcpy(&mBuffer[mWriteCursor], data, bounded_size);
+        }
+        Sirikata::JpegError err = Sirikata::JpegError::nil();
+        if (bounded_size != size) {
+            err = Sirikata::JpegError::errEOF();
+        }
+        mWriteCursor += bounded_size;
+        return std::pair<Sirikata::uint32, Sirikata::JpegError>(bounded_size,
+                                                                err);
+    }
+    std::vector<Sirikata::uint8, JpegAllocator<uint8_t> > &buffer() {
+        return mBuffer;
+    }
+    size_t bytes_written() const{
+        return mWriteCursor;
+    }
+    const std::vector<Sirikata::uint8, JpegAllocator<uint8_t> > &buffer() const{
+        return mBuffer;
+    }
+    bool has_reached_bound() const {
+        return mBuffer.size() <= mWriteCursor;
+    }
+    void write(const void *data, unsigned int size) {
+        Write((const Sirikata::uint8*)data, size);
+    }
+    void write_byte(Sirikata::uint8 data) {
+        Write(&data, 1);
+    }
+};
+}
