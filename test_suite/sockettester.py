b0902fe886773c6a13ce8b3b0af61250ac13ce5d
ChangZhuo Chen (陳昌倬) <czchen@czchen.org>
2016-07-31 20:38:00 +0800

    Ensure proc.terminate() is called after unit test
    Closed: #42

--- test_suite/sockettester.py
+++ test_suite/sockettester.py
@@ -48,72 +48,75 @@ def test_compression(binary_name, socket_name = None, too_short_time_bound=False
     proc = subprocess.Popen(xargs,
                             stdout=subprocess.PIPE,
                             stdin=subprocess.PIPE)
-    socket_name = proc.stdout.readline().strip()
-    if custom_name:
-        # test that we are unable to connect to the subprocess
-        dup_proc = subprocess.Popen(xargs,
-                            stdout=subprocess.PIPE,
-                            stdin=subprocess.PIPE)
-        duplicate_socket_name = b''
-        duplicate_socket_name = dup_proc.stdout.readline().strip()
-        assert (not duplicate_socket_name)
-    if is_zlib:
-        socket_name = socket_name.replace(b'.uport', b'') + b'.z0'
-    with open(jpg_name, 'rb') as f:
-        jpg = f.read()
-    def encoder():
-        try:
-            lepton_socket.sendall(jpg)
-            lepton_socket.shutdown(socket.SHUT_WR)
-        except EnvironmentError:
-            pass
-    def decoder():
-        try:
-            lepton_socket.sendall(dat)
-            lepton_socket.shutdown(socket.SHUT_WR)
-        except EnvironmentError:
-            pass
-
-    t=threading.Thread(target=encoder)
-
-    encode_start = time.time()
-    lepton_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    lepton_socket.connect(socket_name)
-    t.start()
-    dat = read_all_sock(lepton_socket)
-    encode_end = time.time()
-    lepton_socket.close()
-    t.join()
-    
-    print ('encode time ',encode_end - encode_start)
-    print (len(jpg),len(dat))
-
-    while True:
-        v=threading.Thread(target=decoder)
-
-        decode_start = time.time()
-        lepton_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        os.listdir('/tmp')
-        lepton_socket.connect(socket_name)
-        v.start()
-        decode_mid = time.time()
-        ojpg = read_all_sock(lepton_socket)
-        decode_end = time.time()
-        lepton_socket.close()
-        v.join()
+    try:
+        socket_name = proc.stdout.readline().strip()
+        if custom_name:
+            # test that we are unable to connect to the subprocess
+            dup_proc = subprocess.Popen(xargs,
+                                stdout=subprocess.PIPE,
+                                stdin=subprocess.PIPE)
+            duplicate_socket_name = b''
+            duplicate_socket_name = dup_proc.stdout.readline().strip()
+            assert (not duplicate_socket_name)
         if is_zlib:
-            ojpg = zlib.decompress(ojpg)
-        print (len(ojpg))
-        print (len(jpg))
-        assert (ojpg == jpg)
-        print ('decode time ',decode_end - decode_start, '(', decode_mid-decode_start,')')
-        if not parsed_args.benchmark:
-            break
-        
-    print ('yay',len(ojpg),len(dat),len(dat)/float(len(ojpg)), 'parent pid is ',proc.pid)
+            socket_name = socket_name.replace(b'.uport', b'') + b'.z0'
+        with open(jpg_name, 'rb') as f:
+            jpg = f.read()
+        def encoder():
+            try:
+                lepton_socket.sendall(jpg)
+                lepton_socket.shutdown(socket.SHUT_WR)
+            except EnvironmentError:
+                pass
+        def decoder():
+            try:
+                lepton_socket.sendall(dat)
+                lepton_socket.shutdown(socket.SHUT_WR)
+            except EnvironmentError:
+                pass
 
-    proc.terminate()
-    proc.wait()
+        t=threading.Thread(target=encoder)
+
+        encode_start = time.time()
+        lepton_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        lepton_socket.connect(socket_name)
+        t.start()
+        dat = read_all_sock(lepton_socket)
+        encode_end = time.time()
+        lepton_socket.close()
+        t.join()
+
+        print ('encode time ',encode_end - encode_start)
+        print (len(jpg),len(dat))
+
+        while True:
+            v=threading.Thread(target=decoder)
+
+            decode_start = time.time()
+            lepton_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            os.listdir('/tmp')
+            lepton_socket.connect(socket_name)
+            v.start()
+            decode_mid = time.time()
+            ojpg = read_all_sock(lepton_socket)
+            decode_end = time.time()
+            lepton_socket.close()
+            v.join()
+            if is_zlib:
+                ojpg = zlib.decompress(ojpg)
+            print (len(ojpg))
+            print (len(jpg))
+            assert (ojpg == jpg)
+            print ('decode time ',decode_end - decode_start, '(', decode_mid-decode_start,')')
+            if not parsed_args.benchmark:
+                break
+
+        print ('yay',len(ojpg),len(dat),len(dat)/float(len(ojpg)), 'parent pid is ',proc.pid)
+
+    finally:
+        proc.terminate()
+        proc.wait()
+
     assert (not os.path.exists(socket_name))
 
 has_avx2 = False
