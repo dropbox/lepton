c14cbae1e265784662d8c59a5b9c88268a5d6f0b
Jongmin Baek <jongmin@dropbox.com>
2018-07-26 10:18:17 -0700

    Expand blocking read tests further
    Thankfully didn't find any bugs with the new set of tests, though there is some non-determinism. :)

--- src/test_iostream.rs
+++ src/test_iostream.rs
@@ -98,18 +98,19 @@ fn istream_preload_test() {
 }
 
 
-/// Creates IoStream of the given preload length, feeds the given data to the output stream
-/// and attempts to read asynchronously the given length from the inptu stream.
-fn _create_and_read_istream(len: usize,
-                            fill: bool,
-                            data_to_write: &[u8],
-                            preload_len: usize) -> InputResult<usize> {
+/// Creates IoStream of the given preload length and runs a number of operations,
+/// with each operation being either feeding data into the output stream or reading data
+/// asynchronously from the input stream, of the designated length.
+/// Will return the total bytes read, or an error once it is encountered.
+fn _create_and_test_iostream(read_lens: &[usize],
+                             write_lens: &[usize],
+                             fill: bool,
+                             preload_len: usize) -> InputResult<usize> {
     let (mut istream, ostream) = iostream(preload_len);
     let pair = Arc::new((Mutex::<bool>::new(false), Condvar::new()));
     let pair_child = pair.clone();
 
-    let mut data_read: Vec<u8> = vec![0u8; len];
-    let data_to_write_local = data_to_write.to_vec();
+    let read_lens_local = read_lens.to_vec();
     let child = thread::spawn(move || {
         // Notify the main thread that this thread has started.
         let &(ref lock, ref cv) = &*pair_child;
@@ -119,15 +120,22 @@ fn _create_and_read_istream(len: usize,
             cv.notify_one();
         }
 
-        let result = istream.read(&mut data_read, fill, true);
-        // Check that if we read anything, it matches what we feed in.
-        if let Ok(bytes_read) = result {
-            assert!(bytes_read <= data_to_write_local.len());
-            for i in 0..bytes_read {
-                assert_eq!(data_read[i], data_to_write_local[i]);
+        let mut bytes_read_so_far = 0usize;
+        for read_len in read_lens_local {
+            let mut data_read: Vec<u8> = vec![0u8; read_len];
+            let result = istream.read(&mut data_read, fill, false);
+            // Check that if we read anything, the bytes we get is correct.
+            if let Ok(bytes_read) = result {
+                assert!(bytes_read <= read_len);
+                for i in 0..bytes_read {
+                    assert_eq!(data_read[i], bytes_read_so_far as u8);
+                    bytes_read_so_far += 1;
+                }
+            } else {
+                return result;
             }
         }
-        result
+        Ok(bytes_read_so_far)
     });
 
     // Wait for the thread to start up.
@@ -138,30 +146,69 @@ fn _create_and_read_istream(len: usize,
     }
 
     // Write to ostream and wait for the child thread.
-    assert_eq!(ostream.write(data_to_write).unwrap(), data_to_write.len());
+    let mut bytes_written_so_far: usize = 0;
+    for write_len in write_lens {
+        let mut data_to_write: Vec<u8> = vec![0u8; *write_len];
+        for i in 0..data_to_write.len() {
+            data_to_write[i] = bytes_written_so_far as u8;
+            bytes_written_so_far += 1;
+        }
+        assert_eq!(ostream.write(&data_to_write).unwrap(), data_to_write.len());
+    }
     ostream.write_eof().unwrap();
     child.join().expect("Spawned child thread failed")
 }
 
 #[test]
 fn blocking_read_test() {
-    // Prepare the data to write (0, 1, 2, ...)
-    let data = [0u8, 1u8, 2u8, 3u8];
-    for fill in [true, false].iter() {
-        // Try reading in as much as we write.
-        let result = _create_and_read_istream(data.len(), *fill, &data, 1024);
-        assert_eq!(result.unwrap(), data.len());
+    for &preload_len in [2, 3, 4, 5, 6, 1024].iter() {
+        println!("Running with preload {}", preload_len);
+        for &fill in [true, false].iter() {
+            // Try reading in as much as we write.
+            let result = _create_and_test_iostream(&[4], &[4], fill, preload_len);
+            assert_eq!(result.unwrap(), 4);
 
-        // Try reading in less than what we write.
-        let result = _create_and_read_istream(data.len() - 1, *fill, &data, 1024);
-        assert_eq!(result.unwrap(), data.len() - 1);
+            // Try reading in a few chunks.
+            let result = _create_and_test_iostream(&[2, 2], &[4], fill, preload_len);
+            assert_eq!(result.unwrap(), 4);
 
-        // Try reading in more than what we write.
-        let result = _create_and_read_istream(100, *fill, &data, 1024);
-        match fill {
-            true => assert!(result.is_err()),
-            false => assert_eq!(result.unwrap(), data.len()),
-        };
+            // Try writing in a few chunks.
+            let result = _create_and_test_iostream(&[4], &[3, 6], fill, preload_len);
+            let bytes_read = result.unwrap();
+            match fill {
+                true => assert_eq!(bytes_read, 4),
+                false => assert!(3 <= bytes_read && bytes_read <= 4), // non-determistic
+            };
+
+            // Try reading in less than what we write.
+            let result = _create_and_test_iostream(&[3], &[4], fill, preload_len);
+            assert_eq!(result.unwrap(), 3);
+
+            // Try reading in less than what we write, in chunks.
+            let result = _create_and_test_iostream(&[2, 4, 6], &[20], fill, preload_len);
+            let bytes_read = result.unwrap();
+            match fill {
+                true => assert_eq!(bytes_read, 2 + 4 + 6),
+                false => assert!(8 <= bytes_read && bytes_read <= 12), // non-deterministic
+            };
+
+            // Try reading in more than what we write.
+            let result = _create_and_test_iostream(&[5], &[4], fill, preload_len);
+            match fill {
+                true => assert!(result.is_err()),
+                false => assert_eq!(result.unwrap(), 4),
+            };
+
+            // Try reading in more than what we write, in chunks..
+            let result = _create_and_test_iostream(&[9], &[2, 2, 4], fill, preload_len);
+            match fill {
+                true => assert!(result.is_err()),
+                false => {
+                    let bytes_read = result.unwrap();
+                    assert!(4 <= bytes_read && bytes_read <= 8) // non-deterministic
+                }
+            };
+        }
     }
 }
 
